77461429ea8310a0e879f6dc1aa0a7df3209938f
Fahim Rimon <fahimrimon21@gmail.com>
2022-06-30 21:42:53 +0600

    database connected

--- index.js
+++ index.js
@@ -12,4 +12,9 @@ const PORT = process.env.PORT || 5000;
 //use cors
 // app.use(cors());
 
+//connect to mongodb ..
+mongoose.connect(process.env.DB_CONNECT)
+.then(()=> console.log("Database connected"))
+.catch(err => console.log(err))
+
 app.listen(PORT, () => console.log("Server connected"));
