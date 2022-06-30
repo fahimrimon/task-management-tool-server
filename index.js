df318f65c9480841cb039db442026ad621ccf172
Fahim Rimon <fahimrimon21@gmail.com>
2022-06-30 22:52:33 +0600

    Display

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
-// const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
 //use express.json() to get data into json format
@@ -10,19 +10,17 @@ app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
 //use cors
-// app.use(cors());
-
+app.use(cors());
 
 //import routes
-const TodoItemRoute = require('./routes/todoItems');
-
+const TodoItemRoute = require("./routes/todoItems");
 
 //connect to mongodb ..
-mongoose.connect(process.env.DB_CONNECT)
-.then(()=> console.log("Database connected"))
-.catch(err => console.log(err))
+mongoose
+  .connect(process.env.DB_CONNECT)
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.log(err));
 
-
-app.use('/', TodoItemRoute);
+app.use("/", TodoItemRoute);
 
 app.listen(PORT, () => console.log("Server connected"));
